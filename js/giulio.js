3e38b229b8854b618dab85af29014a05e463b912
martaarut <marta.aruta@community.unipa.it>
2024-06-19 12:42:01 +0200

    new

--- js/giulio.js
+++ js/giulio.js
@@ -1,35 +1,31 @@
+let rez = 0.02;
+let t = 0;
+
 function setup() {
-    createCanvas(windowWidth,windowHeight);
-    background (300,200,0)
-    color (RGB)
-    
-    
-  }
-    let x = 0;
-    let y = 0;
-    let size = 40;
-    
+  createCanvas(windowWidth,windowWidth);
   
-  function draw() {
-     strokeWeight(1);
-    
-    if(random(2) < 0.9){
-      stroke (300,200,0,0)
-      fill (200,100,0,random(255))
-      //line(x,y, x+size, y+size, y);
-      
-    } else {
-      stroke (150,0,0,0)
-      fill(150,0,0, random(255))
-      square(x,y,40)
-      //triangle (x, y+size, x+size, y)
+  noLoop();
+}
+
+function draw() {
+  background(255);
+  space = 10;
+  size = 30;
+
+  for (i = 0; i < width; i += space) {
+    for (j = 0; j < height; j += space) {
+      n = noise(i * rez, j * rez, t);
+      //n = random(1);
+      //stroke(n*255,n*255,n*255,255);
+      stroke(0);
+      //fill(n*255,n*255,n*255,255);
+      strokeWeight(3);
+      if (n < 0.5) {
+        line(i, j, i + size, j + size);
+      } else {
+        line(i + size, j, i, j + size);
+      }
     }
-   
-    
-    x += size;
-    
-    if(x> width) {
-      x= 0;
-      y += size
-    }
+    t += 0.00002;
   }
+}
--- js/giulio2.js
+++ js/giulio2.js
@@ -1,27 +1,31 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background (300,200,0) }
-    let x = 0;
-    let y = 0;
-    let size = 30;
-    
-  
-  function draw() {
-     strokeWeight(19);
-    
-    if(random(2) < 1){
-      
-      line(x,y, x+size, y+size, y);
-      
-    } else {
-      
-      line (x, y+size, x+size, y)
-    } 
-    
-    x += size;
-    
-    if(x> width) {
-      x= 0;
-      y += size
-    }
+  createCanvas(windowWidth, windowWidth);
+  background(300, 200, 0);
+}
+
+let x = 0;
+let y = 0;
+let size = 100;
+
+function draw() {
+  strokeWeight(20);
+
+  if (random(1) < 0.5) {
+    fill(255, 0, 0, 200);  // Rosso con opacità
+  } else {
+    fill(0, 0, 255, 200);  // Blu con opacità
   }
+
+  triangle(x, y + size, x + size, y + size, x + size / 2, y);
+
+  x += size;
+
+  if (x >= width) {
+    x = 0;
+    y += size;
+  }
+
+  if (y >= height) {
+    noLoop();  // Ferma il disegno una volta che l'intera tela è riempita
+  }
+}

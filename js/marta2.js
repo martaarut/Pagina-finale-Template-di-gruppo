5f2378054a94839b1c484a2f15537f55c112162b
martaarut <marta.aruta@community.unipa.it>
2024-06-24 11:09:35 +0200

    editing

--- js/marta2.js
+++ js/marta2.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(160, 82, 45);
+  background(191, 189, 188);
   noLoop();
 }
 
@@ -11,11 +11,13 @@ function draw() {
   for (let y = 0; y < height; y += cellSize) {
       for (let x = 0; x < width; x += cellSize) {
           if (random() > 0.5) {
-              stroke(139, 69, 19); // Brown color 1
-              line(x, y, x + cellSize, y + cellSize);
+              stroke(162, 25, 255); // Purple 1
+              // Draw a bezier curve
+              bezier(x, y, x + cellSize / 2, y, x + cellSize / 2, y + cellSize, x + cellSize, y + cellSize);
           } else {
-              stroke(181,101,29); // Brown color 2
-              line(x + cellSize, y, x, y + cellSize);
+              stroke(66, 49, 137); //Purple 2
+              // Draw a bezier curve
+              bezier(x + cellSize, y, x + cellSize / 2, y, x + cellSize / 2, y + cellSize, x, y + cellSize);
           }
       }
   }
